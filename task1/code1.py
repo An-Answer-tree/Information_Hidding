a63921b7e5146f5e1fea4e87886ee78efb76f560
Hanami-Satoi <53655496+Hanami-Satoi@users.noreply.github.com>
2024-11-12 00:01:44 +0800

    feature: add multi audio channel process

--- task1/code1.py
+++ task1/code1.py
@@ -3,21 +3,26 @@ from scipy.io import wavfile
 from scipy.fft import fft, ifft
 import matplotlib.pyplot as plt
 
-# 步骤1: 读取音频文件
-rate, data = wavfile.read('music.wav')  # 替换成你的音频文件名
+# 读取PCM音频文件
+file_path = 'audio_work/sardine_fish_L.wav'  
+sample_rate, audio_channel = wavfile.read(file_path)
 
-# 步骤2: 一维离散傅里叶变换
-data_fft = fft(data)
+# 只选择一个声道进行分析
+if audio_channel.ndim > 1:
+    audio_channel = audio_channel[:, 0]
 
-# 步骤3: 一维离散傅里叶逆变换
+# 一维离散傅里叶变换
+data_fft = fft(audio_channel)
+
+# 一维离散傅里叶逆变换
 data_ifft = ifft(data_fft)
 
-# 步骤4: 观察结果
+# 可视化原始音频波形、频谱图和逆变换后的音频波形
 # 原始音频波形
 plt.figure(figsize=(12, 6))
 plt.subplot(2, 2, 1)
 plt.title("Original Audio Waveform")
-plt.plot(data)
+plt.plot(audio_channel)
 
 # 频谱图
 plt.subplot(2, 2, 2)
