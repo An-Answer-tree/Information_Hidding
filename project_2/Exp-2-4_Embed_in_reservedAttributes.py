28743034082c3669f9a72ac8cd19cd879f88d559
Hanami-Satoi <53655496+Hanami-Satoi@users.noreply.github.com>
2024-11-19 05:09:12 +0800

    feat: add out_of_range detection
    - now too long number will raise error
    - rearrange function and code order
    - rename src file

--- project_2/Exp-2-4_Embed_in_reservedAttributes.py
+++ project_2/Exp-2-4_Embed_in_reservedAttributes.py
@@ -1,13 +1,14 @@
 import struct
 
-def embed_data_in_bmp(bmp_file, data, output_file):
+def embed_in_reserved_attributes(bmp_file, data, output_file):
     with open(bmp_file, 'rb') as file:
         bmp_header = file.read(14)  # 读取文件头部
         rest_of_file = file.read()  # 读取剩余文件内容
 
     # 将数据打包为无符号四字节整数
     packed_data = struct.pack('<I', data)
-
+    if len(packed_data) > 4:
+        raise ValueError("Data is too large to embed, max size is 4 bytes")
     # 生成新的文件头，替换保留字段
     new_header = bmp_header[:6] + packed_data + bmp_header[10:]
 
@@ -18,10 +19,8 @@ def embed_data_in_bmp(bmp_file, data, output_file):
 
     print(f"Data embedded into {output_file}")
 
-# 调用函数，示例中隐藏数字 12345
-embed_data_in_bmp('baboon.bmp', 12345, 'baboon_with_hidden_data.bmp')
 
-def extract_data_from_bmp(bmp_file):
+def extract_data_from_reserved_attributes(bmp_file):
     with open(bmp_file, 'rb') as file:
         file.read(6)  # 跳过文件头的开始部分
         hidden_data = file.read(4)  # 读取隐藏的数据
@@ -31,5 +30,5 @@ def extract_data_from_bmp(bmp_file):
     print(f"Data extracted: {data}")
     return data
 
-# 调用函数，从修改过的文件中提取数据
-extract_data_from_bmp('baboon_with_hidden_data.bmp')
+embed_in_reserved_attributes('./resource/2/baboon.bmp', 12580, './resource/2/baboon3.bmp')
+extract_data_from_reserved_attributes('./resource/2/baboon3.bmp')
