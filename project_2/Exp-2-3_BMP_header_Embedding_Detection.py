50a733419e21c9b18e5af32f6089348f4c1092cd
Hanami-Satoi <53655496+Hanami-Satoi@users.noreply.github.com>
2024-11-19 04:46:49 +0800

    feat: fully record bmp header
    - now the code will record all the header attributes for futher usage
    - rename few srcs

--- project_2/Exp-2-3_BMP_header_Embedding_Detection.py
+++ project_2/Exp-2-3_BMP_header_Embedding_Detection.py
@@ -3,14 +3,15 @@ import struct
 def check_bmp_for_hidden_data(bmp_file):
     with open(bmp_file, 'rb') as f:
         # 读取文件头字段
-        bfType = f.read(2)
+        bfType = struct.unpack('H', f.read(2))[0]
         bfSize = struct.unpack('I', f.read(4))[0]
-        f.read(4)  # 跳过保留位
+        bfReserved1 = struct.unpack('H', f.read(2))[0]
+        bfReserved2 = struct.unpack('H', f.read(2))[0]
         bfOffBits = struct.unpack('I', f.read(4))[0]
         biSize = struct.unpack('I', f.read(4))[0]
         biWidth = struct.unpack('I', f.read(4))[0]
-        biHeight = struct.unpack('I', f.read(4))[0]
-        f.read(2)  # 跳过biPlanes
+        biHeight = struct.unpack('i', f.read(4))[0]
+        biPlanes = struct.unpack('H', f.read(2))[0]
         biBitCount = struct.unpack('H', f.read(2))[0]
 
         # 计算每像素字节数
@@ -31,4 +32,4 @@ def check_bmp_for_hidden_data(bmp_file):
             print("未检测到异常，文件头与数据之间没有明显隐藏信息。")
 
 # 使用函数检查文件
-check_bmp_for_hidden_data('baboon2.bmp')
+check_bmp_for_hidden_data('./resource/2/baboon2.bmp')
